04888630513e9161e345e387fd17631df57036c8
shun-tak <shun.tak221@gmail.com>
2014-01-13 15:18:49 +0900

    ゲーム画面の準備

--- nyanshuffle/Classes/GameScene.cpp
+++ nyanshuffle/Classes/GameScene.cpp
@@ -27,5 +27,49 @@ bool GameScene::init()
         return false;
     }
 
+    srand((unsigned int)time(NULL));
+    int random = rand();
+
+    CCSize size = CCDirector::sharedDirector()->getWinSize();
+
+    // 背景の表示
+    CCSprite* pBG = CCSprite::create("game_bg.png");
+    pBG->setPosition(ccp(size.width * 0.5, size.height * 0.5));
+    this->addChild(pBG);
+
+    CCSize bgSize = pBG->getContentSize();
+
+    // 雲の表示
+    CCSprite* pCloud1 = CCSprite::create("cloud.png");
+    pCloud1->setPosition(ccp(bgSize.width * 0.4, bgSize.height * 0.83));
+    pBG->addChild(pCloud1);
+
+    CCSprite* pCloud2 = CCSprite::create("cloud.png");
+    pCloud2->setPosition(ccp(bgSize.width * 0.2, bgSize.height * 0.9));
+    pCloud2->setScale(0.6);
+    pBG->addChild(pCloud2);
+
+    // ネコの表示
+    CCString* catImagePath = CCString::createWithFormat("cat%d.png", random % 5 + 1);
+    m_pCat = CCSprite::create(catImagePath->getCString());
+    m_pCat->setPosition(ccp(size.width * 0.5, size.height * 0.5));
+    this->addChild(m_pCat, 0);
+
+    // 箱の表示
+    CCString* boxFileName = CCString::createWithFormat("box%d.png", random % 2 + 1);
+    m_pBox1 = CCMenuItemImage::create(boxFileName->getCString(), boxFileName->getCString(), NULL, NULL);
+    m_pBox1->setPosition(ccp(size.width * 0.2, size.height * 1.5));
+
+    m_pBox2 = CCMenuItemImage::create(boxFileName->getCString(), boxFileName->getCString(), NULL, NULL);
+    m_pBox2->setPosition(ccp(size.width * 0.5, size.height * 1.5));
+
+    m_pBox3 = CCMenuItemImage::create(boxFileName->getCString(), boxFileName->getCString(), NULL, NULL);
+    m_pBox3->setPosition(ccp(size.width * 0.8, size.height * 1.5));
+
+    CCMenu* menu = CCMenu::create(m_pBox1, m_pBox2, m_pBox3, NULL);
+    menu->setPosition(CCPointZero);
+
+    this->addChild(menu);
+
     return true;
 }
--- nyanshuffle/Classes/GameScene.h
+++ nyanshuffle/Classes/GameScene.h
@@ -11,8 +11,16 @@
 
 #include "cocos2d.h"
 
+USING_NS_CC;
+
 class GameScene : public cocos2d::CCLayer
 {
+protected:
+    CCSprite* m_pCat;
+    CCMenuItemImage* m_pBox1;
+    CCMenuItemImage* m_pBox2;
+    CCMenuItemImage* m_pBox3;
+
 public:
     virtual bool init();
     static cocos2d::CCScene* scene();
